c60146916e485ba607393ad329b8906dec9ba230
Johannes Raggam <thetetet@gmail.com>
2016-01-12 15:49:44 +0100

    add leaflet functionality from plone.formwidget.geolocation

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
 require.config({
     baseUrl: "src",
     paths: {
-        "Leaflet": "bower_components/leaflet/dist/leaflet-src",
+        "leaflet":           "bower_components/leaflet/dist/leaflet-src",
+        "L.fullscreen":      "bower_components/Leaflet.fullscreen/dist/Leaflet.fullscreen",
+        "L.providers":       "bower_components/leaflet-providers/leaflet-providers",
+        "L.geosearch":       "bower_components/L.GeoSearch/src/js/l.control.geosearch",
+        "L.geosearch.esri":  "bower_components/L.GeoSearch/src/js/l.geosearch.provider.esri",
+        "L.markercluster":   "bower_components/leaflet.markercluster/dist/leaflet.markercluster-src",
+        "L.awesomemarkers":  "bower_components/Leaflet.awesome-markers/dist/leaflet.awesome-markers",
         // BASE DEPENDENCIES
-        "jquery":                       "bower_components/jquery/dist/jquery",
-        "jquery.browser":               "bower_components/jquery.browser/dist/jquery.browser",
-        "logging":                      "bower_components/logging/src/logging",
-        "pat-base":                     "bower_components/patternslib/src/core/base",
-        "pat-compat":                   "bower_components/patternslib/src/core/compat",
-        "pat-jquery-ext":               "bower_components/patternslib/src/core/jquery-ext",
-        "pat-logger":                   "bower_components/patternslib/src/core/logger",
-        "pat-mockup-parser":            "bower_components/patternslib/src/core/mockup-parser",
-        "pat-parser":                   "bower_components/patternslib/src/core/parser",
-        "pat-registry":                 "bower_components/patternslib/src/core/registry",
-        "pat-utils":                    "bower_components/patternslib/src/core/utils",
-        "underscore":                   "bower_components/underscore/underscore"
+        "jquery":            "bower_components/jquery/dist/jquery",
+        "jquery.browser":    "bower_components/jquery.browser/dist/jquery.browser",
+        "logging":           "bower_components/logging/src/logging",
+        "pat-base":          "bower_components/patternslib/src/core/base",
+        "pat-compat":        "bower_components/patternslib/src/core/compat",
+        "pat-jquery-ext":    "bower_components/patternslib/src/core/jquery-ext",
+        "pat-logger":        "bower_components/patternslib/src/core/logger",
+        "pat-mockup-parser": "bower_components/patternslib/src/core/mockup-parser",
+        "pat-parser":        "bower_components/patternslib/src/core/parser",
+        "pat-registry":      "bower_components/patternslib/src/core/registry",
+        "pat-utils":         "bower_components/patternslib/src/core/utils",
+        "underscore":        "bower_components/underscore/underscore"
 
     },
     "shim": {
+        "L.fullscreen": { deps: ["leaflet"] },
+        "L.geosearch": { deps: ["leaflet"] },
+        "L.geosearch.esri": { deps: ["leaflet"] },
+        "L.markercluster": { deps: ["leaflet"] },
+        "L.awesomemarkers": { deps: ["leaflet"] },
         "logging": { "exports": "logging" }
-    }
+    },
+    wrapShim: true
 });
 
 require(["jquery", "pat-registry", "pat-leaflet"], function($, registry, pattern) {
--- src/pat-leaflet.js
+++ src/pat-leaflet.js
@@ -1,4 +1,4 @@
-(function (root, factory) {
+(function(root, factory) {
     if (typeof define === "function" && define.amd) {
         define([
             "jquery",
@@ -6,14 +6,23 @@
             "pat-registry",
             "pat-parser",
             "pat-logger",
-            "Leaflet",
+            "leaflet",
+            "L.fullscreen",
+            "L.providers",
+            "L.geosearch",
+            "L.geosearch.esri",
+            "L.markercluster",
+            "L.awesomemarkers"
         ], function() {
             return factory.apply(this, arguments);
         });
     } else {
         // If require.js is not available, you'll need to make sure that these
         // global variables are available.
-        factory($, patterns.Base, patterns, patterns.Parser, patterns.logger); // jshint ignore:line
+        factory($, patterns.Base, patterns, patterns.Parser, patterns.logger,
+            patterns.Leaflet, patterns.L.fullscreen, patterns.L.providers,
+            patterns.L.geosearch, patterns.L.geosearch.esri,
+            patterns.L.markercluster, patterns.L.awesomemarkers);
     }
 }(this, function($, Base, registry, Parser, logger, L) {
     "use strict";
@@ -22,25 +31,117 @@
     log.debug("pattern loaded");
 
     var parser = new Parser("leaflet");
-    parser.addArgument("mapcenterlat", "50.636");
-    parser.addArgument("mapcenterlon", "5.566");
-    parser.addArgument("mapzoom", "13");
+
+    parser.addArgument("fullscreencontrol", true);
+    parser.addArgument("image_path", "src/bower_components/Leaflet.awesome-markers/dist/images");
+
+    parser.addArgument("editable", false);
+    parser.addArgument("input_latitude_selector", undefined);
+    parser.addArgument("input_longitude_selector", undefined);
 
     return Base.extend({
         name: "leaflet",
         trigger: ".pat-leaflet",
+        map: undefined,
 
         init: function initUndefined () {
             this.options = parser.parse(this.$el);
-            var map = new L.map(this.$el[0]);
-            map.setView([this.options.mapcenterlat, this.options.mapcenterlon], this.options.mapzoom);
 
-            L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-                attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
-            }).addTo(map);
+            var baseLayers,
+                geopoints,
+                markers,
+                bounds,
+                geosearch;
 
-            this.$el.html(this.options.text);
+            // MAP INIT
+            this.map = new L.Map(this.$el[0], {
+                fullscreenControl: this.options.fullscreencontrol
+            });
+
+            L.Icon.Default.imagePath = this.options.image_path;
+
+            // Layers
+            baseLayers = {
+                "Map": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+                "Satellite": L.tileLayer.provider("Esri.WorldImagery"),
+                "Topographic": L.tileLayer.provider("OpenTopoMap"),
+                "Toner": L.tileLayer.provider("Stamen.Toner")
+            };
+            baseLayers.Map.addTo(this.map); // default map
+            L.control.layers(baseLayers).addTo(this.map);
+
+            // ADD MARKERS
+            geopoints = this.$el.data().geopoints;
+            if (geopoints) {
+                markers = this.create_markers(geopoints, this.options.editable);
+                this.map.addLayer(markers);
+
+                // autozoom
+                bounds = markers.getBounds();
+                this.map.fitBounds(bounds);
+            }
+
+            if (this.options.editable) {
+                this.map.on("geosearch_showlocation", function(e) {
+                    if (markers) {
+                        this.map.removeLayer(markers);
+                    }
+                    var coords = e.Location;
+                    this.update_inputs(coords.Y, coords.X);
+                    this.bind_draggable_marker(e.Marker);
+                });
+
+                // GEOSEARCH
+                geosearch = new L.Control.GeoSearch({
+                    showMarker: true,
+                    draggable: this.options.editable,
+                    provider: new L.GeoSearch.Provider.Esri()
+                    //provider: new L.GeoSearch.Provider.Google()
+                    //provider: new L.GeoSearch.Provider.OpenStreetMap()
+                });
+                geosearch.addTo(this.map);
+            }
+
             log.debug("pattern initialized");
+        },
+
+        green_marker: L.AwesomeMarkers.icon({
+            markerColor: "green"
+        }),
+
+        update_inputs: function(lat, lng) {
+            $(this.options.input_latitude_selector).attr("value", lat);
+            $(this.options.input_longitude_selector).attr("value", lng);
+        },
+
+        bind_draggable_marker: function(marker) {
+            marker.on("dragend", function(e) {
+                var coords = e.target.getLatLng();
+                this.update_inputs(coords.lat, coords.lng);
+            });
+        },
+
+        create_markers: function(geopoints, editable) {
+            // return MarkerClusterGroup from geopoints
+            // geopoints = [{lat: NUMBER, lng: NUMBER, popup: STRING}]
+            var markers = new L.MarkerClusterGroup();
+            for (var i = 0, size = geopoints.length; i < size; i++) {
+                var geopoint = geopoints[i],
+                    marker;
+                marker = new L.Marker([geopoint.lat, geopoint.lng], {
+                    icon: this.green_marker,
+                    draggable: editable
+                });
+                if (geopoint.popup) {
+                    marker.bindPopup(geopoint.popup);
+                }
+                if (editable) {
+                    this.bind_draggable_marker(marker);
+                }
+                markers.addLayer(marker);
+            }
+            return markers;
         }
+
     });
 }));
