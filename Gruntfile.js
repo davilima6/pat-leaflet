f61b2950a98d13b08ebed752f9f7e05c48a5a8b9
Johannes Raggam <thetetet@gmail.com>
2016-01-12 17:02:56 +0100

    gruntfile. dont know if we need it, but here it is

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+/* global module */
+module.exports = function(grunt) {
+    "use strict";
+
+    var dest_path = "src/build/";
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        concat: {
+            options: {
+                separator: grunt.util.linefeed + grunt.util.linefeed
+            },
+            js: {
+                src: [
+                    "src/bower_components/leaflet/dist/leaflet-src.js",
+                    "src/bower_components/Leaflet.fullscreen/dist/Leaflet.fullscreen.js",
+                    "src/bower_components/leaflet-providers/leaflet-providers.js",
+                    "src/bower_components/L.GeoSearch/src/js/l.control.geosearch.js",
+                    "src/bower_components/L.GeoSearch/src/js/l.geosearch.provider.esri.js",
+                    "src/bower_components/leaflet.markercluster/dist/leaflet.markercluster-src.js",
+                    "src/bower_components/Leaflet.awesome-markers/dist/leaflet.awesome-markers.js"
+                ],
+                dest: dest_path + "libs.js"
+            },
+            css: {
+                src: [
+                    "src/bower_components/leaflet/dist/leaflet.css",
+                    "src/bower_components/Leaflet.fullscreen/dist/leaflet.fullscreen.css",
+                    "src/bower_components/L.GeoSearch/src/css/l.geosearch.css",
+                    "src/bower_components/leaflet.markercluster/dist/MarkerCluster.Default.css",
+                    "src/bower_components/leaflet.markercluster/dist/MarkerCluster.css",
+                    "src/bower_components/Leaflet.awesome-markers/dist/leaflet.awesome-markers.css"
+                ],
+                dest: dest_path + "libs.css"
+            }
+        },
+
+        uglify: {
+            default: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: dest_path + "libs.min.js.map",
+                    sourceMapIncludeSources: false
+                },
+                files: {
+                    "src/build/libs.min.js": [
+                        dest_path + "libs.js"
+                    ]
+                }
+            }
+        },
+
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: "src/bower_components/leaflet/dist/images/",
+                    src: ["**"],
+                    dest: dest_path + "images/"
+                }, {
+                    expand: true,
+                    cwd: "src/bower_components/Leaflet.fullscreen/dist/",
+                    src: ["*.png"],
+                    dest: dest_path + "images/"
+                }, {
+                    expand: true,
+                    cwd: "src/bower_components/Leaflet.awesome-markers/dist/images/",
+                    src: ["*.png"],
+                    dest: dest_path + "images/"
+                }]
+            }
+        },
+
+        sed: {
+            "leaflet-fullscreen-1": {
+                path: dest_path + "libs.css",
+                pattern: "fullscreen.png",
+                replacement: "images/fullscreen.png"
+            },
+            "leaflet-fullscreen-2": {
+                path: dest_path + "libs.css",
+                pattern: "fullscreen@2x.png",
+                replacement: "images/fullscreen@2x.png"
+            },
+        }
+
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-sed");
+
+    // Default task(s).
+    grunt.registerTask("default", ["concat", "uglify", "copy", "sed"]);
+
+};
